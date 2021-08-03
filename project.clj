08a12f4ec2fd8c0e54523c1d0e6740a96fff0d4f
Jim Clark <jim@atomist.com>
2021-08-02 17:53:28 -0700

    Add a bad dep

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :url "https://github.com/atomisthq/bot-service"
 
   :dependencies [[ring/ring-core "1.8.2"]
-                 [ring/ring-jetty-adapter "1.8.2"]]
+                 [ring/ring-jetty-adapter "1.8.2"]
+                 [org.apache.camel/camel-core "2.15.0"]]
 
   :exclusions [commons-logging log4j org.slf4j/slf4j-log4j12]
 
