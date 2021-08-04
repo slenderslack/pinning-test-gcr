fa73b217d93080c69272f88e5da93eff327c7a1b
Jim Clark <jim@atomist.com>
2021-08-04 14:05:31 -0700

    bump

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :url "https://github.com/atomisthq/bot-service"
 
   :dependencies [[ring/ring-core "1.8.2"]
-                 [ring/ring-jetty-adapter "1.8.2"]]
+                 [ring/ring-jetty-adapter "1.8.2"]
+                 [org.springframework/spring-core "5.0.0.RELEASE"]
+                 ]
 
   :exclusions [commons-logging log4j org.slf4j/slf4j-log4j12]
 
